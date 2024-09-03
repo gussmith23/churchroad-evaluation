5451eccc659ac9d741952b7bb889d970d1484f9e
cknizek <87683288+cknizek@users.noreply.github.com>
2024-09-02 19:56:49 -0700

    Revert "New benchmarks + synthesis flags + encoded features for dataviz (#2)"
    This reverts commit 6a35ae53023b0047c6600632c5366e867f77f603.

--- benchmarks/mul/0_stage/mul_0_stage_unsigned_18_25_43_bit.sv
+++
@@ -1,9 +0,0 @@
-
-(* use_dsp = "yes" *) module mul_0_stage_unsigned_18_25_43_bit(
-  input [17:0] a, 
-  input [24:0] b, 
-  output [42:0] out);
-
-  assign out = a * b;
-  
-endmodule
--- benchmarks/mul/0_stage/mul_0_stage_unsigned_32_32_64_bit.sv
+++
@@ -1,9 +0,0 @@
-
-(* use_dsp = "yes" *) module mul_0_stage_unsigned_32_32_64_bit(
-  input [31:0] a, 
-  input [31:0] b, 
-  output [63:0] out);
-
-  assign out = a * b;
-  
-endmodule
